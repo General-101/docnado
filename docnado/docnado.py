cd9f44e4d9ae524058eceb71ab6fd355274bbefa
Steven Garcia <stevengarcia55@ymail.com>
2020-10-23 20:11:56 -0500

    Add more markdowns

--- docnado/docnado.py
+++ docnado/docnado.py
@@ -383,12 +383,25 @@ class MultiExtension(Extension):
 
 # https://python-markdown.github.io/extensions/
 mdextensions = [MultiExtension(),
-                'markdown.extensions.tables',
-                'markdown.extensions.meta',
+                'markdown.extensions.extra',
+                'markdown.extensions.abbr',
+                'markdown.extensions.attr_list',
                 'markdown.extensions.def_list',
-                'markdown.extensions.headerid',
                 'markdown.extensions.fenced_code',
-                'markdown.extensions.attr_list']
+                'markdown.extensions.footnotes',
+                'markdown.extensions.md_in_html',
+                'markdown.extensions.tables',
+                'markdown.extensions.admonition',
+                'markdown.extensions.codehilite',
+                'markdown.extensions.legacy_attrs',
+                'markdown.extensions.legacy_em',
+                'markdown.extensions.meta',
+                'markdown.extensions.nl2br',
+                'markdown.extensions.sane_lists',
+                'markdown.extensions.smarty',
+                'markdown.extensions.toc',
+                'markdown.extensions.wikilinks',
+                'markdown.extensions.headerid']
 
 
 def build_meta_cache(root):
