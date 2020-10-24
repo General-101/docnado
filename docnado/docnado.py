ddf4a2da595e22068b00ff3d87d21573082fa303
Steven Garcia <stevengarcia55@ymail.com>
2020-10-23 20:19:38 -0500

    Turns out it didn't like that

--- docnado/docnado.py
+++ docnado/docnado.py
@@ -383,25 +383,13 @@ class MultiExtension(Extension):
 
 # https://python-markdown.github.io/extensions/
 mdextensions = [MultiExtension(),
-                'markdown.extensions.extra',
-                'markdown.extensions.abbr',
-                'markdown.extensions.attr_list',
-                'markdown.extensions.def_list',
-                'markdown.extensions.fenced_code',
-                'markdown.extensions.footnotes',
-                'markdown.extensions.md_in_html',
                 'markdown.extensions.tables',
-                'markdown.extensions.admonition',
-                'markdown.extensions.codehilite',
-                'markdown.extensions.legacy_attrs',
-                'markdown.extensions.legacy_em',
                 'markdown.extensions.meta',
-                'markdown.extensions.nl2br',
-                'markdown.extensions.sane_lists',
-                'markdown.extensions.smarty',
-                'markdown.extensions.toc',
-                'markdown.extensions.wikilinks',
-                'markdown.extensions.headerid']
+                'markdown.extensions.def_list',
+                'markdown.extensions.headerid',
+                'markdown.extensions.fenced_code',
+                'markdown.extensions.attr_list',
+                'markdown.extensions.toc']
 
 
 def build_meta_cache(root):
